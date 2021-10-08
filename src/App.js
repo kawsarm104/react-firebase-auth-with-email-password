22031bc726df5ef027e13fd00c092c7688b8f7f5
kawsarm104 <kawsarm104@gmail.com>
2021-10-08 10:40:25 +0600

    .............

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
+import initializeAuthentication from "./firebase/firebase.initialize";
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const auth = getAuth();
+  const handleEmailOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordOnChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleRegistration = (e) => {
+    console.log(email, password);
+    e.preventDefault();
+
+    if (password.length < 6) {
+      setError("password must be at least 6 charecter");
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+        setError("");
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+        // ..
+      });
+  };
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        setError("");
+      })
+      .catch((error) => {
+        setError(error.message);
+        // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // The email of the user's account used.
+        // const email = error.email;
+        // // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-5 ">
+      <form
+        className="w-75 shadow border m-2 p-3 "
+        onSubmit={handleRegistration}
+      >
+        <h1 className="text-center text-primary">Signup form</h1>
+        <div className="row mb-3 mt-5 ">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">
+            Email
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="email"
+              onBlur={handleEmailOnChange}
+              className="form-control"
+              id="inputEmail3"
+              required
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">
+            Password
+          </label>
+          {/* Error message for password  */}
+
+          <div className="col-sm-10">
+            <div className="text-danger">{error}</div>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword3"
+              onBlur={handlePasswordOnChange}
+              required
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck1"
+              />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="d-flex justify-content-between">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleGoogleSignin}
+          >
+            Google Signin
+          </button>
+          <button type="submit" className="btn btn-primary">
+            Sign in
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnj7usKODbv5KBfx-Ly7kjHOygggEHc7Y",
+  authDomain: "react-firebase-auth-63390.firebaseapp.com",
+  projectId: "react-firebase-auth-63390",
+  storageBucket: "react-firebase-auth-63390.appspot.com",
+  messagingSenderId: "839937442982",
+  appId: "1:839937442982:web:a7d98c0dda0fabacbee067",
+};
+
+export default firebaseConfig;
